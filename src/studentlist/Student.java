6c486d5a6439a2b594b7a2fff1fa74293300f837
Maryam <khatibzm@sheridancollege.ca>
2022-01-24 16:25:42 +0400

    first commit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,27 @@
+
+package studentlist;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Maryam Khatib
+ */
+public class Student {
+       
+      private String stud_Name;
+
+    /**
+     * @return the stud_Name
+     */
+    public String getStud_Name() {
+        return stud_Name;
+    }
+
+    /**
+     * @param stud_Name the stud_Name to set
+     */
+    public void setStud_Name(String stud_Name) {
+        this.stud_Name = stud_Name;
+    }
+      
+}
---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,34 @@
+package studentlist;
+
+import java.util.Scanner;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author MaryamKhatib
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Student[] sList = new Student[3]; //creating an array of object
+
+        Scanner input = new Scanner(System.in)
+                
+        for (int i = 0; i <= sList.length - 1; i++) {
+
+            sList[i]= new Student();
+            System.out.println("Enter student name: ");
+            sList[i].setStud_Name(input.nextLine());
+        }
+        
+        for (int i = 0; i <= sList.length - 1; i++) {
+                 
+       System.out.println("Student Name: " + sList[i].getStud_Name());
+        }
+    }
+
+}
