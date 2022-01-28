bfcda45f6101c7ddf58df7f68899cf90530b88a0
Maryam <khatibzm@sheridancollege.ca>
2022-01-28 13:49:53 +0400

    branch created

---
+++ src/studentlist/ParttimeStudent.java
@@ -0,0 +1,27 @@
+
+package studentlist;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Maryam Khatib
+ */
+public class ParttimeStudent extends Student {
+   private int numOfCourses;
+
+    /**
+     * @return the numOfCourses
+     */
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    /**
+     * @param numOfCourses the numOfCourses to set
+     */
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+   
+}
+
